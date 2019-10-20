b94b5722dac1cc6740226cf377b96f5dd02049e4
Boris Golomb <boris.golomb@ihsmarkit.com>
2019-10-20 16:59:04 +0300

    feat: add description of projects to mobile version

---
+++ script_description.js
@@ -0,0 +1,31 @@
+document.querySelector('#first_pr_button').addEventListener('click', function(){
+
+    if(document.querySelectorAll('.description--text')[0].classList.contains('show_description')){
+        document.querySelectorAll('.description--text')[0].classList.remove('show_description')
+    } else {
+        document.querySelectorAll('.description--text')[0].classList.add('show_description');
+    }
+
+});
+
+document.querySelector('#second_pr_button').addEventListener('click', function(){
+
+     if(document.querySelectorAll('.description--text')[1].classList.contains('show_description')){
+         document.querySelectorAll('.description--text')[1].classList.remove('show_description')
+     } else {
+         document.querySelectorAll('.description--text')[1].classList.add('show_description');
+     }
+ 
+ });
+
+ window.addEventListener('resize', function(){
+    
+    document.querySelectorAll('.description--text').forEach(
+        project=>{
+            if(project.classList.contains('show_description')){
+                project.classList.remove('show_description')
+            }
+        }
+    )
+
+ });
